512f1582c3b212c111ff942a6c013f64b38aaef7
dparret <damienparret@hotmail.com>
2025-01-06 20:46:04 +0100

    New feature: Save with same time

--- src/app/components/Record.tsx
+++ src/app/components/Record.tsx
@@ -9,35 +9,48 @@ interface RecordProps {
 }
 
 const Record: React.FC<RecordProps> = ({elapsedTime, places, setPlaces, settings}) => {
-      // To record a new number and its time
-  const recordNumber = (raceField: HTMLFormElement) => {
-    const updatedPlaces = [...places, {position: places.length + 1, raceNumber: raceField.value, time: elapsedTime}]
+  // To record a new number and its time
+  const recordNumber = (raceField: HTMLFormElement, withSameTime: boolean) => {
+    const updatedPlaces = withSameTime
+      ? [...places, {position: places.length + 1, raceNumber: raceField.value, time: places[places.length - 1].time}]
+      : [...places, {position: places.length + 1, raceNumber: raceField.value, time: elapsedTime}]
+    
     // Save the places in local storage
     localStorage.places = JSON.stringify(updatedPlaces);
     setPlaces(updatedPlaces);
   }
 
-  const saveRaceNumber = () => {
+  const saveRaceNumber = (withSameTime: boolean) => {
     const raceField = document.getElementById('number') as HTMLFormElement;
-    if (isRaceFieldValid(raceField, places, settings)) recordNumber(raceField);
+    if (isRaceFieldValid(raceField, places, settings)) recordNumber(raceField, withSameTime);
     raceField.value = ""
   }
 
   return (
-    <div className="flex flex-row gap-2 w-full" >
+    <div className="flex flex-col gap-2 w-full" >
       <input 
         type="number"
         id="number"
         className="grow text-white text-sm bg-gray-700 border border-gray-600 rounded-md focus:ring-blue-500 focus:border-blue-500 h-10 p-2.5 placeholder-gray-400"
         placeholder={`Race number (${settings.min}-${settings.max})`}
       />
-      <button 
-        type="button" 
-        className="text-white py-2 px-4 rounded-md bg-green-600 disabled:bg-gray-400 hover:bg-green-700" 
-        onClick={() => saveRaceNumber()}
-      >
-      Record
-      </button>
+      <div className="flex flex-row gap-2 w-full" >
+        <button 
+          type="button" 
+          className="grow text-white py-2 px-4 rounded-md bg-green-800 disabled:bg-gray-500 hover:bg-green-700" 
+          onClick={() => saveRaceNumber(true)}
+          disabled={places.length === 0}
+        >
+        Same Time
+        </button>
+        <button 
+          type="button" 
+          className="grow text-white py-2 px-4 rounded-md bg-green-600 disabled:bg-gray-500 hover:bg-green-700" 
+          onClick={() => saveRaceNumber(false)}
+        >
+        Record
+        </button>
+      </div>
     </div>
   );
 }
